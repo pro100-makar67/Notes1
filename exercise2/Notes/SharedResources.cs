b051822c7851d6f313245ff0a6c0cae4caa2a0cb
shitikov-me <shitikme17@gmail.com>
2026-06-16 15:11:34 +0700

    Заметки 2

---
+++ exercise2/Notes/SharedResources.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes
+{
+    static class SharedResources
+    {
+        public static readonly Color FontColor = Color.FromRgb(0, 0, 0);
+        public static readonly Color BackgroundColor = Color.FromRgb(255, 0, 0);
+    }
+}
